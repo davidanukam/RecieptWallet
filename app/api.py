469c36958c75341fb7035474a74436f9c780416e
Duzzenn <109883788+davidanukam@users.noreply.github.com>
2025-12-27 00:17:06 -0500

    Fixed output of api

--- app/api.py
+++ app/api.py
@@ -1,5 +1,4 @@
-import requests
-import os
+import requests, os
 
 from dotenv import load_dotenv
 
@@ -7,30 +6,29 @@ load_dotenv()
 
 url = "https://ocr43.p.rapidapi.com/v1/results"
 
-payload = '''-----011000010111000001101001r
-Content-Disposition: form-data; name="image"r
-r
-gcc-commands.jpgr
------011000010111000001101001r
-Content-Disposition: form-data; name="url"r
-r
-https://storage.googleapis.com/api4ai-static/samples/ocr-1.pngr
------011000010111000001101001--r
-r
-'''
-
 headers = {
 	"x-rapidapi-key": os.environ.get("X_RAPIDAPI_KEY"),
 	"x-rapidapi-host": "ocr43.p.rapidapi.com",
-	"Content-Type": "multipart/form-data; boundary=---011000010111000001101001"
 }
 
+file_path = "./app/images/test_reciept.png"
+
 try:
-    response = requests.post(url, data=payload, headers=headers)
+    with open(file_path, "rb") as image_file:
+        files = {"image": image_file}
+        response = requests.post(url, files=files, headers=headers)
+
     if response.status_code == 200:
-        print(response.json())
+        data = response.json()
+        # print(data)
+        text = data['results'][0]['entities'][0]['objects'][0]['entities'][0]['text']
+        print(text)
     else:
         error_detail = response.json().get('detail')
         print(f"Error ({response.status_code}): {error_detail}")
 except requests.exceptions.ConnectionError:
     print("Error: Could not connect to the API")
+except FileNotFoundError:
+    print("Error: The local image file was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
