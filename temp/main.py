303c924995ac9071324b65133b958414cc139a12
Duzzenn <109883788+davidanukam@users.noreply.github.com>
2025-12-27 20:11:45 -0500

    Removed unnecessary folders and files

--- temp/main.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from src.api import get_output
-from src.db import save_output
-
-if __name__ == "__main__":
-    # get_output()
-    save_output()
-    # uvicorn.run("src.app:app", host="0.0.0.0", port=8000)
